7c9040790599bb4660ec53b35e7f63f6fe486296
getCamBill <57048957+getCamBill@users.noreply.github.com>
2019-10-27 16:28:42 +0100

    Ajout du dico avec objet

--- gameClass.py
+++ gameClass.py
@@ -1,15 +1,17 @@
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+  def __init__(self, name: str = "John", age: int = 22):
+      self.name: str = name
+      self.age: int = age
 
   def myfunc(self):
     print("Hello my name is " + self.name)
 
-p1 = Person("John", 36)
+p1 = Person()
 p2 = Person("Bill", 42)
 
-Tablier = dict({'A1': None, 2: p2})
+Tablier = dict({'A1': None, 'A2': p2})
 print(Tablier['A1'])
+print(Tablier['A2'].name)
 Tablier['A1'] = p1
 print(Tablier['A1'].name)
+print(Tablier['A2'].name)
