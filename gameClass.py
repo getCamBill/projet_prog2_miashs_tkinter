39f93c3e02b6f252c683af747697e9cffe5a022a
getCamBill <57048957+getCamBill@users.noreply.github.com>
2019-10-27 16:59:16 +0100

    Test

--- gameClass.py
+++ gameClass.py
@@ -1,4 +1,15 @@
-print("Test pour Git")
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-class test(object):
-    print("test")
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p2 = Person("Bill", 42)
+
+Tablier = dict({'A1': None, 2: p2})
+print(Tablier['A1'])
+Tablier['A1'] = p1
+print(Tablier['A1'].name)
