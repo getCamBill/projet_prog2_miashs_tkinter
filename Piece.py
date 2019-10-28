6db8ad790ff1eb0331cde009de016cdfe5e0a83b
getCamBill <57048957+getCamBill@users.noreply.github.com>
2019-10-28 13:27:33 +0100

    MAJ des classes Piece et Tablier

--- Piece.py
+++ Piece.py
@@ -10,7 +10,17 @@ class Piece(object):
     def getID(self):
         id :str = self.color[0] + self.shape[0] + self.size[0] + str(self.dug)
         return id
+    def getColor(self):
+        return self.color
 
+    def getShape(self):
+        return self.shape
+
+    def getDug(self):
+        return self.dug
+
+    def getSize(self):
+        return self.size
 
     def showCarac(self):
         print("Couleur : ", self.color, '\n'
---
+++ Tablier.py
@@ -0,0 +1,29 @@
+from Pioche import Pioche
+from Piece import Piece
+from Case import Case
+
+class Tablier(object):
+
+    def __init__(self):
+
+        self.tablier = dict({}) # tablier de 16 cases avec id et valeur de la piece posée
+        #-------------------------------
+        self.i: int = 0
+        for lettre in ("ABCD"):
+            for val in ("1234"):
+                self.i += 1
+                self.id = lettre+val
+                case = Case(self.id)
+                self.tablier[self.i] = {case: None}
+
+    def showTablier(self):
+        print(self.tablier.values())
+
+    #def isCaseLibre(self, case: str): # case est définie par 'B2' par ex
+
+
+    #def poserPiece(self, case: str, pieceID: str):
+
+
+t1 = Tablier()
+t1.showTablier()
