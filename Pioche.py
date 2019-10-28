2d2a701e2a18831348345207ff51e7d80e2a74a2
getCamBill <57048957+getCamBill@users.noreply.github.com>
2019-10-28 13:26:45 +0100

    Ajout de la classe Pioche

--- Pioche.py
+++ Pioche.py
@@ -1,21 +1,22 @@
 from Piece import Piece
+# La Pioche est un objet qui contient une liste d'objet 'Piece'
 
 class Pioche(object):
 
     def __init__(self):
-        i: int = 0
-        #self.pioche = dict({}) # la pioche de piece est une liste d'objet
-        self.pioche = []
+        self.i: int = 0
+
+        self.Pioche = []
         for color in ("Marron", "Beige"):
             for shape in ("Rond", "Carré"):
                 for size in ("Grand", "Petit"):
                     for dug in (1, 0):
                         piece: Piece = Piece(color, shape, size, dug)
-                        self.pioche.append(piece)
-                        i += 1
+                        self.Pioche.append(piece)
+                        self.i += 1
 
     def showPiece(self):
-        for pce in self.pioche:
+        for pce in self.Pioche:
             print(pce.getID())
 
 
