9a8538119dbee775dfcb1300ed5b14918e4a7a66
getCamBill <57048957+getCamBill@users.noreply.github.com>
2019-10-30 22:08:20 +0100

    Classe Piece

--- Piece.py
+++ Piece.py
@@ -6,14 +6,3 @@ class Piece(object):
         self.shape: str = shape
         self.size: str = size
         self.dug: int = dug
-
-    def getID(self):
-        self.idPiece :str = self.color[0] + self.shape[0] + self.size[0] + str(self.dug)
-        return self.idPiece
-
-    def showCarac(self):
-        print("Couleur : ", self.color, '\n'
-              "Forme : ", self.shape, '\n'
-              "Taille : ", self.size, '\n'
-              "Creux : ", self.dug
-              )
