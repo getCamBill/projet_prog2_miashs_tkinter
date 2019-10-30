5350e3a3aa7b964b5035bb949bb32a13560bc1d2
getCamBill <57048957+getCamBill@users.noreply.github.com>
2019-10-30 22:09:21 +0100

    Classe Etat, à finir pour les différents état du jeu (égalité, victoire, etc)

--- Etat.py
+++ Etat.py
@@ -1,10 +1,6 @@
-from Piece import Piece
-from Tablier import Tablier
-
 class Etat(object):
 
     def __init__(self, etatInitial: bool = False):
         self.etatInitial = etatInitial
         self.victoire = False
         self.egalite = False
-
