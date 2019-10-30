33203f6c1334e2e84d56131d3c614e51b7a98101
getCamBill <57048957+getCamBill@users.noreply.github.com>
2019-10-30 13:21:22 +0100

    classe Pioche a dégrossir

--- Pioche.py
+++ Pioche.py
@@ -6,23 +6,41 @@ class Pioche(object):
     def __init__(self):
         self.i: int = 0
 
-        self.Pioche = []
+        self.Pioche = dict({})
         for color in ("Marron", "Beige"):
             for shape in ("Rond", "Carré"):
                 for size in ("Grand", "Petit"):
                     for dug in (1, 0):
+                        self.idPiece: str = color[0] + shape[0] + size[0] + str(dug)
                         piece: Piece = Piece(color, shape, size, dug)
-                        self.Pioche.append(piece)
+                        self.Pioche[self.idPiece] = piece
                         self.i += 1
 
-    def showPiece(self):
-        for pce in self.Pioche:
-            print(pce.getID())
+    def showPieces(self):
+        for ele in self.Pioche.items():
+            print(ele)
+
+    def __getitem__(self, item):
+        try:
+            return self.Pioche[item]
+        except:
+            print("La pièce est déjà posée ! ")
+
+    def __setitem__(self, key, value):
+        del self.Pioche[key]
 
 
+# p1 = Pioche()
+# #p1.showPieces()
+# piece = Piece()
+# print(p1['MRG0'])
+# print("Pièce vide",piece)
+# piece = p1.__getitem__('MRG0')
+# print(p1.__getitem__('MRG0'))
+# p1.__setitem__('MRG0', None)
+# #p1.showPieces()
+# print("Piece vide est devenu : ",piece)
 
-p1 = Pioche()
-#p1.showPiece()
 
 
 
