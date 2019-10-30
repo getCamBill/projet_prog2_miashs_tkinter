e6125cd5033f08c4243453f19d1e0680a33e93fb
getCamBill <57048957+getCamBill@users.noreply.github.com>
2019-10-30 14:32:47 +0100

    MAJ des classes :

---
+++ gameClass.py
@@ -0,0 +1,17 @@
+from Tablier import Tablier
+
+
+class Game(object):
+
+    #def __init__(self):
+
+    def start(self):
+        print("  A   B   C   D")
+        print("1")
+        print("2")
+        print("3")
+        print("4")
+
+
+if __name__ == "__main__":
+    Game().start()
